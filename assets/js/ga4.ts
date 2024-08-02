ef2b990587cdd99fe71289c1a5a764a088d597d3
Abdullah Türkmen <abdullahtrkmn13@gmail.com>
2024-08-02 19:31:13 +0300

    style & click functions updated

---
+++ assets/js/ga4.ts
@@ -0,0 +1,69 @@
+export const sendGA4Events = ({
+  campaign,
+  measurementId,
+  apiSecretKey,
+  ga4ClientId,
+}: {
+  campaign: string;
+  measurementId?: string;
+  apiSecretKey?: string;
+  ga4ClientId?: string;
+}) => {
+  const GA4_CLIENT_ID = ga4ClientId || Math.random().toString(36).substring(7);
+  const ANALYTICS_ID = measurementId || "G-7PNZ5E4JDZ";
+  const API_SECRET = apiSecretKey || "RU0cCzDxRbCbpV6xOZ7xQA";
+
+  async function sendGA4Event(name: string, action: string, label: string) {
+    return await fetch(
+      `https://www.google-analytics.com/mp/collect?measurement_id=${ANALYTICS_ID}&api_secret=${API_SECRET}`,
+      {
+        method: "POST",
+        body: JSON.stringify({
+          client_id: GA4_CLIENT_ID,
+          events: [
+            {
+              name: action,
+              params: {
+                event_category: name,
+                event_label: label,
+              },
+            },
+          ],
+        }),
+      }
+    );
+  }
+
+  function pushEvent(action: string, label: string) {
+    console.debug("pushEvent", { label, campaign, action });
+
+    sendGA4Event(campaign, action, label)
+      .then((data) => {
+        console.debug("sendGA4Events", data.status);
+      })
+      .catch((e) => console.error(e));
+  }
+
+  return {
+    pushEvent,
+    sendItemImpression: (label: string) =>
+      pushEvent("item_impression", `impression:${label}`),
+    sendItemClick: (label: string) =>
+      pushEvent("item_impression", `click:${label}`),
+  };
+};
+
+/**
+ * Example Usage:
+ * const { sendItemImpression, sendItemClick } = sendGA4Events({ campaign: "bundle-lines-bunaltan-sicaklar-20240713" });
+ *
+ * Click:
+ * ...
+ * <button onClick={() => sendItemClick("visit_ad")}>Go To Ad</button>
+ *
+ * Impression:
+ * ...
+ * document.addEventListener("DOMContentLoaded", (event: Event) => {
+ *   sendItemImpression("homepage");
+ * });
+ */
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
+    const title = document.title;
+
+    //share modal toggle
     const shareButton = document.getElementById('shareButton');
     const shareContainer = document.getElementById('shareContainer');
 
@@ -10,5 +13,43 @@ document.addEventListener('DOMContentLoaded', () => {
         if (!shareContainer.contains(event.target) && event.target !== shareButton) {
             shareContainer.classList.add('hidden');
         }
+    });
+
+    //copy url
+    const copyButton = document.getElementById('copyLink');
+    copyButton.addEventListener('click', async () => {
+        const url = window.location.href;
+        await navigator.clipboard.writeText(url)
+            .then(() => {
+                console.log('URL copied to clipboard!');
+            })
+            .catch(err => {
+                console.error('Failed to copy: ', err);
+            });
+    });
+
+    //share whatsapp
+    const whatsappButton = document.getElementById('shareWhatsapp');
+    whatsappButton.addEventListener('click', () => {
+        const url = window.location.href;
+        const whatsappUrl = `https://wa.me/?text=${encodeURIComponent(title)} | ${encodeURIComponent(url)}`;
+        window.open(whatsappUrl, '_blank');
+    });
+
+    //share facebook
+    const facebookButton = document.getElementById('shareFacebook');
+    facebookButton.addEventListener('click', () => {
+        const url = window.location.href;
+        const facebookUrl = `https://www.facebook.com/sharer/sharer.php?u=${encodeURIComponent(url)}&quote=${encodeURIComponent(title)}`;
+        window.open(facebookUrl, '_blank');
+    });
+
+
+    // share x
+    const xButton = document.getElementById('shareX');
+    xButton.addEventListener('click', () => {
+        const url = window.location.href;
+        const xUrl = `https://x.com/intent/tweet?url=${encodeURIComponent(title)}-${encodeURIComponent(url)}`;
+        window.open(xUrl, '_blank');
     });
 });
